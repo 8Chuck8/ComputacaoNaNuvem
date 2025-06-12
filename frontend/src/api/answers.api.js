75268b547bc238f788a237b24eb277b735bc927c
Diogo aka. Chuck <90628978+8Chuck8@users.noreply.github.com>
2025-06-12 03:44:12 +0100

    Update answers.api.js

--- frontend/src/api/answers.api.js
+++ frontend/src/api/answers.api.js
@@ -6,7 +6,7 @@ export const useAnswerApi = create((set) => ({
     answers: [],
     setAnswers: (answers) => set({answers}),
     getAnswers: async () => {
-        const res = await fetch("${API_URL}/api/answers");
+        const res = await fetch(`${API_URL}/api/answers`);
         const data = await res.json();
         console.log("Fetched answers:", data);
         set({answers: data.data});
@@ -16,7 +16,7 @@ export const useAnswerApi = create((set) => ({
             return {success: false, message: "Please provide all fields"};
         }
 
-        const res = await fetch("${API_URL}/api/answers", {
+        const res = await fetch(`${API_URL}/api/answers`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json" 
