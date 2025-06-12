1da8f0db2f2081fe715a243e397b295857249459
Diogo aka. Chuck <90628978+8Chuck8@users.noreply.github.com>
2025-06-12 02:41:46 +0100

    Update questions.api.js

--- frontend/src/api/questions.api.js
+++ frontend/src/api/questions.api.js
@@ -1,10 +1,12 @@
 import {create} from 'zustand'
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export const useQuestionApi = create((set) => ({
     questions: [],
     setQuestions: (questions) => set({questions}),
     getQuestions: async () => {
-        const res = await fetch("/api/questions");
+        const res = await fetch("${API_URL}/api/questions");
         const data = await res.json();
         set({questions: data.data});
     },
@@ -13,7 +15,7 @@ export const useQuestionApi = create((set) => ({
             return {success: false, message: "Please provide all fields"};
         }
 
-        const res = await fetch("/api/questions", {
+        const res = await fetch("${API_URL}/api/questions", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json" 
