4e8ac734453aa20c762b7a06633b5108186bbec0
Diogo aka. Chuck <90628978+8Chuck8@users.noreply.github.com>
2025-06-12 03:45:05 +0100

    Update users.api.js

--- frontend/src/api/users.api.js
+++ frontend/src/api/users.api.js
@@ -6,7 +6,7 @@ export const useUserApi = create((set) => ({
     users: [],
     setUsers: (users) => set({users}),
     getUsers: async () => {
-        const res = await fetch("${API_URL}/api/users");
+        const res = await fetch(`${API_URL}/api/users`);
         const data = await res.json();
         set({users: data.data});
     },
@@ -15,7 +15,7 @@ export const useUserApi = create((set) => ({
             return {success: false, message: "Please provide all fields"};
         }
 
-        const res = await fetch("${API_URL}/api/users", {
+        const res = await fetch(`${API_URL}/api/users`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json" 
