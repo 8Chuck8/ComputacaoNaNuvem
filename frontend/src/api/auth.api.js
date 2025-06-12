50c0d910c0aa54e4f4b045153990c3930c61b61e
Diogo aka. Chuck <90628978+8Chuck8@users.noreply.github.com>
2025-06-12 22:01:22 +0100

    Update auth.api.js

--- frontend/src/api/auth.api.js
+++ frontend/src/api/auth.api.js
@@ -9,7 +9,7 @@ export const useAuthApi = create((set) => ({
         console.log("EMAIL", email);
         console.log("PASSWORD", password);
 
-        const res = await fetch("/api/users/login", {
+        const res = await fetch(`${API_URL}/api/users/login`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -29,7 +29,7 @@ export const useAuthApi = create((set) => ({
             return {success: false, message: "Please provide all fields"};
         }
 
-        const res = await fetch("/api/users", {
+        const res = await fetch(`${API_URL}/api/users`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json" 
